600d9d6df12ae7f59f42ebdbd9fcb6935697e127
me school <hoang.tien.2016@skola.ssps.cz>
2020-01-06 17:03:29 +0100

    in progress

--- WordHunter/WordHunter/Program.cs
+++ WordHunter/WordHunter/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace WordHunter
 {
@@ -7,6 +9,15 @@ namespace WordHunter
     {
         static void Main(string[] args)
         {
+            string path = "C:\\Users\\hoang.tien.2016\\source\\repos\\WordHunter\\WordHunter\\loremipsum.txt";
+
+            string[] arr = File.ReadAllText(path).Split(" ");
+            string[] arrClean = arr.Distinct().ToArray();
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
         }
     }
 }
